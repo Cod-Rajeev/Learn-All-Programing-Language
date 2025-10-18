a994a7c7ee6f59c0fe07ea03a5997e942b5fd1de
Cod-Rajeev <rajeevrajindian@gmail.com>
2025-10-18 10:56:02 +0530

    fotter

--- src/Website/Footer/Footer.js
+++ src/Website/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div>
-        <div className='bg-secondary text-center py-5'>&copy; Copyright 2024</div>
+        <div className='bg-secondary text-center py-1'>&copy;2025 RAjeev Kumar</div>
     </div>
   )
 }
